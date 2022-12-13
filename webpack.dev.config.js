582a9fc084101bbd2f4fc359be73b5654701360c
K0nfigMDA <iamjustadreamer@yandex.ru>
2022-12-13 15:11:47 +0300

    init: base configs

---
+++ webpack.dev.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        // contentBase: path.resolve(__dirname, 'dist'),
+				static: {
+					directory: path.join(__dirname, 'dist'),
+				},
+				compress: true,
+				port: 3000,
+    },
+};
