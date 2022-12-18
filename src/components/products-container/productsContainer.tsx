6ea8575f073357848774f9889ca352bab9153975
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2022-12-18 16:05:02 +0300

    feat: add filter state to productsContainer

--- src/components/products-container/productsContainer.tsx
+++ src/components/products-container/productsContainer.tsx
@@ -1,16 +1,54 @@
 import { useProducts } from "../../hooks/useProducts";
 import Product from "../product/product";
 import './productsContainer.scss';
+import { useSearchParams } from "react-router-dom";
+
+interface IQueryStringOptions {
+  [key: string]: string[] | undefined;
+}
 
 export default function ProductsContainer() {
   const {products, error} = useProducts();
+  const [searchParams] = useSearchParams();
+  
+  const queryParams: IQueryStringOptions = {};
+  searchParams.forEach((v, k) => {
+    queryParams[k] = v.split('↕');
+  })
+
   return (
-    <div className="products-container">
-      { 
-        error ? 
-        error :
-        products.map(product => <Product product={product} key={product.id}/>) 
-      }
-    </div>
+      <div className="products-container">
+        { 
+          error 
+          ? error 
+          : products
+            .filter((prod) => {
+              const inCategory = queryParams.category 
+              ? queryParams.category.some(el => el.toLowerCase() === prod.category.toLowerCase())
+              : true;
+
+              const inBrand = queryParams.brand 
+              ? queryParams.brand.some(el => el.toLowerCase() === prod.brand.toLowerCase())
+              : true;
+
+              const inPrice = queryParams.price 
+              ? (
+                prod.price >= Number(queryParams.price[0]) &&
+                prod.price <= Number(queryParams.price[1])
+                )
+              : true;
+
+              const inStock = queryParams.stock 
+              ? (
+                prod.stock >= Number(queryParams.stock[0]) &&
+                prod.stock <= Number(queryParams.stock[1])
+                )
+              : true;
+
+              return (inCategory && inBrand && inPrice && inStock);
+            })
+            .map(product => <Product product={product} key={product.id}/>) 
+        }
+      </div>
   );
 }
