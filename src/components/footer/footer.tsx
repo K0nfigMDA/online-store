b87b203b53b68647482dc7bbb57e4dd12410a57a
K0nfigMDA <iamjustadreamer@yandex.ru>
2022-12-18 14:56:43 +0300

    feat: add layout footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,21 @@
+import './footer.scss'
+
+function Footer() {
+
+  return (
+		<footer className='footer'>
+			<div className='footer__git-links'>
+				<a href="https://github.com/K0nfigMDA">
+					<img src="" alt="" />
+				</a>
+				<a href="https://github.com/Nikozyrev">
+					<img src="" alt="" />
+				</a>
+			</div>
+			<div className='footer__rs-link'></div>
+			<div className='footer__yeear'>2022</div>
+		</footer>
+  );
+}
+
+export default Footer;
