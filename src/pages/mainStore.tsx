b1e4c8f8562e0fabdaebecc645d589f29a373075
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2022-12-19 13:04:52 +0300

    feat: add basic checkbox filters

--- src/pages/mainStore.tsx
+++ src/pages/mainStore.tsx
@@ -1,7 +1,15 @@
-
+import FiltersContainer from "../components/filters/filtersContainer";
+import ProductsContainer from "../components/products-container/productsContainer";
+import { useProducts } from "../hooks/useProducts";
+import './mainStore.scss';
 
 export default function MainStore() {
+  const {products, error} = useProducts();
+
   return (
-    <div>Main - TODO</div>   
+    <div className="store-page">
+      <FiltersContainer products={products}/>
+      <ProductsContainer products={products} error={error}/>
+    </div>
   );
 }
