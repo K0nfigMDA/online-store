98394a6a52741c32a98100b32c684cd201e68030
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2022-12-21 12:45:11 +0300

    feat: add cart context

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
 import Header from '../header/header';
-
 import {Routes, Route} from 'react-router-dom';
 import CartPage from '../../pages/cart';
 import MainStore from '../../pages/mainStore';
 import ProductDetails from '../../pages/productDetails';
+import { CartProvider } from '../../contexts/cart/cartContext';
 
 
 function App() {
   return (
 		<>
-			<Header/>
-      <Routes>
-        <Route path='/' element={ < MainStore /> }></Route>
-        <Route path='/cart' element={ < CartPage /> }></Route>
-        <Route path='/product-details' element={ < ProductDetails /> }></Route>
-      </Routes>
+      <CartProvider>
+        <Header/>
+        <Routes>
+          <Route path='/' element={ < MainStore /> }></Route>
+          <Route path='/cart' element={ < CartPage /> }></Route>
+          <Route path='/product-details' element={ < ProductDetails /> }></Route>
+        </Routes>
+      </CartProvider>
     </>
   );
 }
