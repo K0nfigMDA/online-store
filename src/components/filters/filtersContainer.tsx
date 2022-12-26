3cfb8b37a642f28809b4ef9c215951a83e568856
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2022-12-26 11:51:21 +0300

    feat: add reset filters button

--- src/components/filters/filtersContainer.tsx
+++ src/components/filters/filtersContainer.tsx
@@ -1,5 +1,6 @@
 import FilterDualSlider from "./dualSlider";
 import FilterList from "./filterList";
+import { useSearchParams } from "react-router-dom";
 
 enum FILTERS {
   category = 'category',
@@ -10,8 +11,25 @@ enum FILTERS {
 }
 
 export default function FiltersContainer() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const resetFiltersHandler = () => {
+    const params = searchParams;
+    
+    for (const param of Array.from(params.keys())) {
+      if (param in FILTERS) {
+        params.delete(param);
+      }
+    }
+    setSearchParams(params);
+  }
+
   return (
     <div className="filters">
+      <div className="filters__control">
+        <button className="button" onClick={resetFiltersHandler}>Reset Filters</button>
+        
+      </div>
       <FilterList filterName={FILTERS.category}/>
       <FilterList filterName={FILTERS.brand}/>
       <FilterDualSlider filterName={FILTERS.price}/>
