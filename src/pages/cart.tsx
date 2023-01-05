b90d9fc7042e26c6b23aea8f5825801627c91acd
K0nfigMDA <iamjustadreamer@yandex.ru>
2023-01-05 13:41:35 +0300

    feat: create empty cart page

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,22 +1,21 @@
-import CartProducts from '../components/cart-products/cart-products'
-import CartSummary from  '../components/cart-summary/cart-summary'
-import './cart.scss'
+import CartProducts from '../components/cart-products/cart-products';
+import CartSummary from '../components/cart-summary/cart-summary';
+import './cart.scss';
 import { useCart } from '../contexts/cart/cartContext';
 
-
-
-
 export default function CartPage() {
-	const { cart } = useCart();
-	console.log(cart)
+   const { cart } = useCart();
 
-	return (
-		<div className='cart__container'>
-			<CartProducts/>
-			<CartSummary/>
-		</div>
-  );
+   return (
+      <div className="cart__container">
+         {cart.length ? (
+            <>
+               <CartProducts /> 
+							 <CartSummary />
+            </>
+         ) : (
+            <div className="empty-card">Cart is Empty</div>
+         )}
+      </div>
+   );
 }
-
-
-
